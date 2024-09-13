840110cdd65886993fd8f5d82ae632d224b9e61a
Eva <eblack50@stuy.edu>
2024-09-13 11:01:49 -0400

    add krewes

---
+++ 04_rando-dict/krewes.py
@@ -0,0 +1,33 @@
+# Eva Black
+# TummyAKE
+# SoftDev
+# 2024-09-13
+# Time: 10:50
+
+import random
+
+def func(krewes):
+    x = random.randrange(0, len(krewes))
+    pd = krewes.keys()[x]
+    students = krewes[pd]
+    x = random.randrange(0, len(students))
+    student = students[x]
+    return student
+
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+print (func(krewes))
+
+
